fcc2792050db1de66484c7a9235d0ba12fa5d52f
Geoffrey Andrade <geoffandrade@hotmail.com>
2020-08-14 15:49:08 -0400

    connected app to hosted DB

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://gax615:hpRb95fQ4cacGcrG@cluster0-shard-00-00.umro0.mongodb.net:27017,cluster0-shard-00-01.umro0.mongodb.net:27017,cluster0-shard-00-02.umro0.mongodb.net:27017/pizzaProj?ssl=true&replicaSet=atlas-aorcnw-shard-0&authSource=admin&retryWrites=true&w=majority'
+};
